fa866d99d70c4b8c7af0e6552cc33f8a0c0a3ae1
v-Alqorith <vahapcandongel@hotmail.com>
2023-06-01 22:58:38 +0100

    Added burger menu for small devices

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,12 +1,12 @@
 export default function Navigation() {
   return (
-    <div className="w-full h-[9vh] flex justify-evenly items-center p-4 bg-white">
+    <div className="w-full h-[9vh] flex justify-evenly items-center p-4 bg-white sm:justify-between">
       <div>
         <div className="text-[12px] text-right">Courses</div>
         <div className="font-bold text-[25px] ">Vahap Can Dongel</div>
       </div>
 
-      <div className="flex justify-center items-center gap-8">
+      <div className="flex justify-center items-center gap-8 sm:hidden">
         <input
           type="search"
           placeholder="Search a course"
@@ -20,6 +20,14 @@ export default function Navigation() {
             Blog
           </li>
         </ul>
+      </div>
+
+      <div className="hidden sm:flex">
+        <svg width={46} height={46} className="fill-black" viewBox="0 0 24 24">
+          <path d="M20.05 11H3.95a.95.95 0 0 0-.95.95v.1c0 .525.425.95.95.95h16.1a.95.95 0 0 0 .95-.95v-.1a.95.95 0 0 0-.95-.95Z" />
+          <path d="M20.05 16H3.95a.95.95 0 0 0-.95.95v.1c0 .525.425.95.95.95h16.1a.95.95 0 0 0 .95-.95v-.1a.95.95 0 0 0-.95-.95Z" />
+          <path d="M20.05 6H3.95a.95.95 0 0 0-.95.95v.1c0 .525.425.95.95.95h16.1a.95.95 0 0 0 .95-.95v-.1a.95.95 0 0 0-.95-.95Z" />
+        </svg>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import CourseCard from "./components/CourseCard";
 export default function Home() {
   return (
     <div className="w-full flex items-center justify-center">
-      <div className="w-3/4 p-4 flex flex-col items-center gap-16">
-        <div className="grid grid-cols-3 gap-8">
+      <div className="w-3/4 p-4 flex flex-col items-center gap-16 sm:w-full">
+        <div className="grid grid-cols-3 gap-8 sm:flex sm:flex-col sm:items-center">
           <CourseCard />
           <CourseCard />
           <CourseCard />
