fd41801c381f2c5e4481e31f17575238f09751d2
v-Alqorith <vahapcandongel@hotmail.com>
2023-06-01 22:37:43 +0100

    Added badge

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,9 +1,17 @@
 export default function Navigation() {
   return (
     <div className="w-full h-[9vh] flex justify-evenly items-center p-4 bg-white">
-      <div className="font-bold text-[25px] ">Vahap Can Dongel</div>
+      <div>
+        <div className="text-[12px] text-right">Courses</div>
+        <div className="font-bold text-[25px] ">Vahap Can Dongel</div>
+      </div>
 
       <div className="flex justify-center items-center gap-8">
+        <input
+          type="search"
+          placeholder="Search a course"
+          className="p-4 focus:outline-none border w-[250px]"
+        />
         <ul className="flex gap-6 justify-center items-center font-semibold cursor-pointer  ">
           <li>
             <a href="https://www.vahapcandongel.co.uk/">Portfolio</a>
@@ -12,12 +20,6 @@ export default function Navigation() {
             Blog
           </li>
         </ul>
-
-        <input
-          type="search"
-          placeholder="Search a course"
-          className="p-4 focus:outline-none border w-[250px]"
-        />
       </div>
     </div>
   );
