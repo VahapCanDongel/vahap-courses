403f8d619c44741efe2838f4509aa8808574a988
v-Alqorith <vahapcandongel@hotmail.com>
2023-06-01 21:34:38 +0100

    commit

---
+++ src/app/components/Navigation.js
@@ -0,0 +1,12 @@
+export default function Navigation() {
+  return (
+    <div className="w-full h-[20%] flex justify-evenly items-center p-4">
+      <div className="font-bold text-[25px]">Vahap Can Dongel</div>
+
+      <ul className="flex gap-2 justify-center items-center font-semibold">
+        <li>Portfolio</li>
+        <li>Blog</li>
+      </ul>
+    </div>
+  );
+}
